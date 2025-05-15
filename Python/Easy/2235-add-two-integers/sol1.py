d6549d252eed37fec7ad0fbf6deaeda585e8e67a
Naman Jain <jain.naman.22560@gmail.com>
2025-05-15 21:06:23 +0530

    Add solution for LeetCode 2235 - Add Two Integers (Python, sol1)

---
+++ Python/Easy/2235-add-two-integers/sol1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        """
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        """
+        return num1 + num2
+    
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.sum(12,5))
